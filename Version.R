744c747387585b25e918398747e7d4d0c8d16112
bevthewitch <bev.dodds2@phs.scot>
2024-07-23 16:27:55 +0100

    Separated out the footnotes text into Footnotes.R. Separated out the Version table into Version.R. Removed the footnote text from app.R and Average gestation at booking.R.

---
+++ Version.R
@@ -0,0 +1,57 @@
+# builds Version table
+
+`Version` <- c("1.0", "1.1", "1.2", "1.3", "1.4")
+`Date` <- c("3 Oct 2023", "9 Nov 2023", "15 Feb 2024", "2 Apr 2024", "2 Jul 2024")
+`Change` <- c("First public release of SPBAND",
+              "Amended Home - How to use this dashboard",
+              "Updated links and standardised titles, labels and metadata",
+              "Corrected the medians and shifts for NHS Forth Valley and NHS Tayside in the ‘Gestation at booking’ measure;
+              replaced CSV download files with accessible Excel download files;
+              updated links and standardised titles, labels, legends and metadata",
+              "Added aggregated values for the Island Boards in the ‘Gestation at termination’ measure - these Boards are now also represented on the Multi Indicator Overview for this measure;
+              revised the y-axis scales for the Island Boards in the small multiple charts (where possible) to make the mainland Boards' variation easier to see;
+              removed the 'dots' from the monthly small multiple charts (i.e. the ‘Gestation at booking’ and ‘Gestation at termination’ measures); added notes describing the issue with NHS Borders planned and unplanned caesarean birth rates"
+)
+
+version_info <- tibble(`Version`, `Date`, `Change`)
+
+output$version_tbl <- renderTable(version_info, 
+                                  striped = TRUE,
+                                  bordered = TRUE)
+
+output$version_panel <- renderUI({
+  
+  fluidRow(
+
+    box(solidHeader = TRUE,
+        width = 12,
+        
+        br(),
+        
+        column(12,
+               
+               p("This dashboard is scheduled to be updated each quarter, generally by the first Tuesday in the months of January, April, July and October."
+               ),
+               
+               p("Data are shown for up to and including the most recent period for which records are considered near complete. Data for the most recent period should be viewed as provisional. Data for the whole time period shown will be refreshed every time the dashboard updated, and data for the most recent periods are likely to change slightly as additional or updated records are added."
+               ),
+               
+               br(),
+               
+               p(paste0("The data were last refreshed on ", pretty_refresh_date, ".")
+               ),
+               
+               br(),
+               
+               p("Small changes to the format of the dashboard are noted with new version numbers as detailed below:"),
+               
+               br(),
+               
+               tableOutput('version_tbl')
+        )
+        
+    ) # box
+    
+  ) # fluidRow
+
+})
