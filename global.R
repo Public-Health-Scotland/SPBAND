88629a56dd4be20023342835321bb7e2cdb3ba6e
Bev Dodds <52245506+bevthewitch@users.noreply.github.com>
2024-12-17 10:41:01 +0000

    Update global.R

--- global.R
+++ global.R
@@ -30,13 +30,13 @@ credentials <- readRDS("admin/credentials.rds")
 # change each quarter:
 # refresh_date =  the date the SMR02, ABC, Terminations data are refreshed, used on each dashboard chart page - autopopulates them
 
-refresh_date <- as.Date("2024-09-17") 
+refresh_date <- as.Date("2024-12-13") 
 
 pretty_refresh_date <- format(refresh_date,"%d %B %Y")
 
 # latest NRS publication date
 
-NRS_published_date <- "17 September 2024"
+NRS_published_date <- "10 December 2024"
 
 # folder for Excel downloads
 
